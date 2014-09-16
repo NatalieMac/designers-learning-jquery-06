0f34685cd24a485d2763f4d2b58aa10243580ac2
Natalie MacLees <natalie@nataliemac.com>
2014-09-16 13:39:23 -0700

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+$('body').removeClass('jsOff').addClass('jsOn');
+
+var accordion = $('.accordion');
+
+accordion.find('dd').hide();
+
+accordion.find('dt').on('click', function(){
+	$(this).toggleClass('open').next('dd').slideToggle().siblings('dd:visible').slideUp().prev('dt').removeClass('open');
+});
